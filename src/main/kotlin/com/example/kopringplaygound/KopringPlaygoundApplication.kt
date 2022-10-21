9031c5501d31289e14dabd66a2ca3c34faff943e
김은호 <uno.kim@tripstore.kr>
2022-10-21 18:44:31 +0900

    init spring boot

---
+++ src/main/kotlin/com/example/kopringplaygound/KopringPlaygoundApplication.kt
@@ -0,0 +1,11 @@
+package com.example.kopringplaygound
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class KopringPlaygoundApplication
+
+fun main(args: Array<String>) {
+    runApplication<KopringPlaygoundApplication>(*args)
+}
