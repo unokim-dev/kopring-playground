30e35f91249da92f57515210d3415402247d7a61
김은호 <uno.kim@tripstore.kr>
2023-03-17 13:50:48 +0900

    db 설정: 로컬에 mysql 세팅 및 코틀린용 jpa 세팅 추가

--- build.gradle.kts
+++ build.gradle.kts
@@ -30,6 +30,7 @@ dependencies {
     implementation("org.thymeleaf.extras:thymeleaf-extras-springsecurity5")
     developmentOnly("org.springframework.boot:spring-boot-devtools")
     runtimeOnly("com.h2database:h2")
+    runtimeOnly("com.mysql:mysql-connector-j")
     testImplementation("org.springframework.boot:spring-boot-starter-test")
     testImplementation("org.springframework.security:spring-security-test")
 }
@@ -43,4 +44,11 @@ tasks.withType<KotlinCompile> {
 
 tasks.withType<Test> {
     useJUnitPlatform()
+}
+
+// kotlin-jpa plugin 의 설정. 아래 annotation 이 적용된 class 는 컴파일 시 `open` 키워드가 적용된다.
+allOpen {
+    annotation("javax.persistence.Entity")
+    annotation("javax.persistence.MappedSuperclass")
+    annotation("javax.persistence.Embeddable")
 }
