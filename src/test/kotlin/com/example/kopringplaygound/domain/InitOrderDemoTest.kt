c151ddec92bd15ff54b2ca2bd6aa69fea2b74c48
김은호 <uno.kim@tripstore.kr>
2023-06-09 23:46:00 +0900

    초기화 순서 테스트

---
+++ src/test/kotlin/com/example/kopringplaygound/domain/InitOrderDemoTest.kt
@@ -0,0 +1,11 @@
+package com.example.kopringplaygound.domain
+
+import org.junit.jupiter.api.Test
+
+class InitOrderDemoTest {
+
+    @Test
+    fun test() {
+        val a = InitOrderDemo("This is test.")
+    }
+}
