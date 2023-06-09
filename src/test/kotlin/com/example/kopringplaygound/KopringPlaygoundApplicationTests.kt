b0f1a349b0f9709a1458a7381e695889dd83b44f
김은호 <uno.kim@tripstore.kr>
2023-06-09 23:53:03 +0900

    부트 테스트
    잭슨 매퍼 테스트인 듯

--- src/test/kotlin/com/example/kopringplaygound/KopringPlaygoundApplicationTests.kt
+++ src/test/kotlin/com/example/kopringplaygound/KopringPlaygoundApplicationTests.kt
@@ -1,12 +1,30 @@
 package com.example.kopringplaygound
 
+import com.fasterxml.jackson.annotation.JsonProperty
+import com.fasterxml.jackson.annotation.JsonValue
+import com.fasterxml.jackson.databind.ObjectMapper
 import org.junit.jupiter.api.Test
+import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
 
 @SpringBootTest
-class KopringPlaygoundApplicationTests {
+class KopringPlaygoundApplicationTests(@Autowired private val objectMapper: ObjectMapper) {
 
     @Test
-    fun contextLoads() {
+    fun enumTest() {
+        println(objectMapper.writeValueAsString(TestClass("test", 10, Grade.A)))
+        println(objectMapper.readValue("{\"name\":\"test\",\"age\":10,\"testValue\":\"잘했네\"}", TestClass::class.java))
+    }
+
+    data class TestClass(
+        val name: String,
+        val age: Int,
+        @JsonProperty("testValue") val grade: Grade,
+    )
+
+    enum class Grade(@JsonValue val description: String) {
+        A("잘했네"),
+        B("했네"),
+        C("그러네"),
     }
 }
