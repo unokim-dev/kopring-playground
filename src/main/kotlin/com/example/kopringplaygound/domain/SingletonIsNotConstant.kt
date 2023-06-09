ca0d77d6d803a4ab321e7f8960c51628114e3541
김은호 <uno.kim@tripstore.kr>
2023-06-09 23:46:44 +0900

    싱글턴은 상수가 아님을 확인하는 코드

---
+++ src/main/kotlin/com/example/kopringplaygound/domain/SingletonIsNotConstant.kt
@@ -0,0 +1,9 @@
+package com.example.kopringplaygound.domain
+
+object SingletonIsNotConstant {
+    val I_AM_NOT_CONSTANT = "asdf"
+
+    fun asdfasdf() {
+        println(SingletonIsNotConstant.I_AM_NOT_CONSTANT)
+    }
+}
