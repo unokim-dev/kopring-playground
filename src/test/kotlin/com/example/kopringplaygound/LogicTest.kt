35a0e883ed358c426e4d6b819ba0f74b64088fd4
김은호 <uno.kim@tripstore.kr>
2023-06-09 23:49:32 +0900

    로직 테스트 업데이트
    코루틴 포함해서 이거저거 테스트한 것

--- src/test/kotlin/com/example/kopringplaygound/LogicTest.kt
+++ src/test/kotlin/com/example/kopringplaygound/LogicTest.kt
@@ -1,9 +1,83 @@
 package com.example.kopringplaygound
 
+import com.example.kopringplaygound.domain.MyClass
+import kotlinx.coroutines.CoroutineExceptionHandler
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.cancel
+import kotlinx.coroutines.coroutineScope
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.runBlocking
+import org.assertj.core.api.Assertions
 import org.junit.jupiter.api.Test
+import java.lang.RuntimeException
 import java.time.LocalDate
+import java.time.LocalDateTime
+import java.time.Period
 
 internal class LogicTest {
+
+    @Test
+    fun substringTest() {
+        val short = "hello there"
+        val long  = "hello there 123456789 123456789 123456789 123456789 123456789 123456789"
+        println(long.take(20))
+        println(short.take(20))
+    }
+
+    @Test
+    fun qusetionMarkTest() {
+        val a: QuestionFields? = null
+        val asdf: Long? = null
+
+        println(a?.wow(100))
+    }
+
+    data class QuestionFields(val name: String, val money: Int) {
+        fun wow(x: Int) = money == x
+    }
+
+    @Test
+    fun asdfasdF() {
+        println(LocalDate.of(2022, 2, 4).isBefore(LocalDate.of(2022, 2, 3)))
+    }
+
+    @Test
+    fun stringContains() {
+        println("asdfasdf".contains("asdf"))
+    }
+
+    @Test
+    fun timerTest() {
+        val ceh = CoroutineExceptionHandler { _, e ->
+            println("asdfasdf, $e")
+        }
+
+//        val job = CoroutineScope(Dispatchers.IO).launch(ceh) {
+//            repeat(5) {
+//                println("hello world")
+//                delay(1000L)
+//                throw RuntimeException("썸띵롱")
+//            }
+//        }
+
+        runBlocking {
+            routine()
+            delay(4_000L)
+//            job.cancel()
+        }
+    }
+
+    suspend fun routine() {
+        repeat(5) {
+            println("hello world")
+            delay(1000L)
+//            throw RuntimeException("썸띵롱")
+        }
+    }
+
     @Test
     fun test() {
         val travels = RelatedTravels(
@@ -23,6 +97,22 @@ internal class LogicTest {
         println(pair)
         println(pair.joinToString())
         println(pair.toString())
+    }
+
+    @Test
+    fun joinString() {
+        val list = listOf("I", "am", "your", "father")
+        println(list.joinToString())
+    }
+
+    @Test
+    fun assertjTest() {
+        val myClass = MyClass("test", MyClass.InnerClass("Hi"))
+
+        Assertions.assertThat(myClass)
+            .hasFieldOrPropertyWithValue("str", "test")
+            .hasFieldOrPropertyWithValue("inner.name", "Hi")
+            .hasFieldOrPropertyWithValue(MyClass::inner.name, "Hi")
     }
 
     data class RelatedTravels(
